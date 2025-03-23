618971ea96637f16769b61c636354a69b37ffba8
avelando <avelarrodrigues89@gmail.com>
2025-03-23 16:20:59 -0300

    Correção na urls.py

--- avare/urls.py
+++ avare/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from importer import views as import_views
 from compare import views as compare_views
 
 urlpatterns = [
     path("", compare_views.start, name="compare-start"),
-    path("compare/questions/", compare_views.compare_detail, name="compare-detail"),
+    path("compare/", include("compare.urls")),
     path("save_response/", import_views.save_response, name="save-response"),
     path("complete/", import_views.complete, name="complete"),
 ]
